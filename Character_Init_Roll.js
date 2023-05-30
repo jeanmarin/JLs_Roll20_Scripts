6d05d254a7e23b11acb2650babb8aac4bef87fbf
Jean-Louis Marin <AlienAIOverlord@gmail.com>
2023-05-30 10:48:30 -0400

    Create Character_Init_Roll.js
    Simple Initiative Roll for Characters on the Script Bar.

---
+++ Character_Init_Roll.js
@@ -0,0 +1,6 @@
+--/ Character Initiative Roll. 
+--/ Add to script Bar and select character and click the button remove this comment line
+
+/em Rolls For initiative
+
+Initiative [[1d20 + @{selected|initiative_bonus} &{tracker}]]
